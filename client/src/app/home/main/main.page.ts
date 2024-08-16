44f05c849d23f9915b24e3a4b3ece71046d8309d
Chittebabu <chittebabu@email.com>
2024-08-16 18:38:51 +0800

    added search functionality

--- client/src/app/home/main/main.page.ts
+++ client/src/app/home/main/main.page.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { IonInput, NavController } from '@ionic/angular';
 
 import { FoodService } from '../../services/food.service';
 import { Food } from '../../interfaces/food';
+import { catchError, debounceTime, distinctUntilChanged, EMPTY, filter, fromEvent, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
-export class MainPage implements OnInit {
+export class MainPage implements OnInit, AfterViewInit {
+  @ViewChild('searchInput', { static: false }) searchInput!: IonInput;
   food!: Food[];
+  filteredFood!: Food[];
   vendorNames: { [key: number]: string } = {};
+  searchQuery$!: Observable<String>;
 
   constructor(
     private navCtrl: NavController,
@@ -22,6 +26,33 @@ export class MainPage implements OnInit {
     this.getAllFood();
   }
 
+  ngAfterViewInit(): void {
+    console.log(this.searchInput);
+    // use rxjs to handle search input
+    this.searchInput.getInputElement().then((inputElement) => {
+      if (inputElement) {
+        fromEvent(inputElement, 'input').pipe(
+          map((event: any) => event.target.value.toLowerCase()),
+          tap((event) => console.log(event)),
+          debounceTime(300), // wait for 300ms before handling next event
+          distinctUntilChanged(), // only look for unique values
+          catchError(() => EMPTY) // return nothing if error
+        ).subscribe({
+          next: (searchTerm: string) => {
+            this.filterFoodList(searchTerm);
+          },
+          error: (err) => {
+            throw new Error(err);
+          }
+        });
+      } else {
+        console.log('No inputs detected...');
+      }
+    }).catch((err) => {
+      throw new Error(err);
+    });
+  }
+
   onMapClick() {
     this.navCtrl.navigateForward('/home/map');
   }
@@ -30,6 +61,7 @@ export class MainPage implements OnInit {
     this.foodService.getAllFood().subscribe({
       next: (response: Food[]) => {
         this.food = response;
+        this.filteredFood = [...this.food];
         
         this.food.forEach(item => {
           if (item.image_url === 'http://localhost:4000/uploads/null') {
@@ -53,6 +85,36 @@ export class MainPage implements OnInit {
       error: (error) => {
         console.log(error);
       }
+    });
+  }
+
+  filterFoodList(searchTerm: string) {
+    if (searchTerm) {
+      this.filteredFood = this.food.filter((food: Food) => {
+        return food.food_name.toLowerCase().includes(searchTerm);
+      });
+    } else {
+      this.filteredFood = [...this.food];
+    }
+  }
+
+  onSearch() {
+    // use rxjs to handle search input
+    this.searchInput.getInputElement().then((inputElement) => {
+      fromEvent(inputElement, 'ionInput').pipe(
+        map((event: any) => event.target.value.toLowerCase()),
+        debounceTime(300), // wait for 300ms before handling next event
+        distinctUntilChanged(), // only look for unique values
+        catchError(() => EMPTY) // return nothing if error
+      ).subscribe({
+        next: (searchTerm: string) => {
+          this.filterFoodList(searchTerm);
+          console.log(this.filteredFood);
+        },
+        error: (err) => {
+          throw new Error(err);
+        }
+      });
     });
   }
 
